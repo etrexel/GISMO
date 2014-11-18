63f22df73372a4fbc61a2d56808479aed1d8faf2
Eric Trexel <etrexel@san.rr.com>
2014-11-18 01:57:58 -0600

    Added the beginning pieces of the UI. Added image files containing tile sets for use in UI. Added test CSV to populate battlefield.

---
+++ js/tiletest.js
@@ -0,0 +1,50 @@
+var game = new Phaser.Game(1000, 500, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render });
+
+function preload() {
+    game.load.tilemap('map', 'battlefield.csv', null, Phaser.Tilemap.CSV);
+    game.load.image('tiles', 'img/terrain_small.png');
+}
+
+var map;
+var layer;
+var cursors;
+var tileSize = 8;
+var mapWidth = 250;
+var mapHeight = 250;
+var moveSpeed = 10;
+
+function create() {
+    map = game.add.tilemap('map', 8, 8);
+    map.addTilesetImage('tiles');
+    layer = map.createLayer(0);
+    layer.resizeWorld();
+
+    cursors = game.input.keyboard.createCursorKeys();
+
+}
+
+function update() {
+
+    if (cursors.left.isDown)
+    {
+        game.camera.x -= moveSpeed;
+    }
+    else if (cursors.right.isDown)
+    {
+        game.camera.x += moveSpeed;
+    }
+
+    if (cursors.up.isDown)
+    {
+        game.camera.y -= moveSpeed;
+    }
+    else if (cursors.down.isDown)
+    {
+        game.camera.y += moveSpeed;
+    }
+
+}
+
+function render() {
+
+}
