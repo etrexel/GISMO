41212fb2c2462e7816d5890fd5cf96fecebb9a03
michaelfschem@gmail.com <michaelfschem@gmail.com>
2014-11-18 09:45:21 -0600

    Tile

---
+++ src/Tile.js
@@ -0,0 +1,25 @@
+var Tile = {
+	terrain = "",
+	unit = "",
+	smoke = "",
+
+	Terrain : function(){
+
+	}
+
+	getTerrain : function(){
+
+	}
+
+	getUnit : function(){
+
+	}
+
+	setUnit : function() {
+
+	}
+
+	hasSmoke : function() {
+		
+	}
+}
