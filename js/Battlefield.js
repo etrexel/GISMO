9d0a31930b2c0ea4d0d15c98885dca6a2227297a
etrexel <etrexel@san.rr.com>
2014-11-19 20:41:51 -0600

    Updated Battlefield class for object oriented approach. Changed some variables within the class.

--- js/Battlefield.js
+++ js/Battlefield.js
@@ -1,49 +1,55 @@
 var Battlefield = function(first_faction, second_faction){
-	var battlefield = [];
-	var faction1 = first_faction;
-	var faction2 = second_faction;
-	var faction1Base = new Location(0, 0);
-	var faction2Base = new Location(0, 0);
-	var faction1Orders = "";
-	var faction2Order = "";
-
-	this.generateBattlefield = function(){
-
-	};
-
-	this.setupTeam = function(in1, st1){
-
-	};
-
-	this.setupTeam = function(in1){
-
-	};
-
-	this.tankReport = function(in1){
-
-	};
-
-	this.objectReport = function(in1, st1){
-
-	};
-
-	this.loadOrders = function(in1, st1){
-
-	};
-
-	this.executeorders = function(){
-
-	};
-
-	this.clearOrders = function(){
-
-	};
-
-	this.updateBattlefield = function(){
-
-	};
-
-	this.checkvictory = function(){
-		
-	};
+	this.battlefield = [];
+	this.faction1 = first_faction;
+	this.faction2 = second_faction;
+	this.faction1Base = "";
+	this.faction2Base = "";
+	this.faction1Tanks = [];
+	this.faction2Tanks = [];
+	this.faction1Orders = "";
+	this.faction2Orders = "";
 };
+
+Battlefield.prototype.generateBattlefield = function(){
+
+};
+
+Battlefield.prototype.setupTeam = function(in1, st1){
+
+};
+
+Battlefield.prototype.setupTeam = function(in1){
+
+};
+
+Battlefield.prototype.tankReport = function(in1){
+
+};
+
+Battlefield.prototype.objectReport = function(in1, st1){
+
+};
+
+Battlefield.prototype.loadOrders = function(in1, st1){
+
+};
+
+Battlefield.prototype.executeorders = function(){
+
+};
+
+Battlefield.prototype.clearOrders = function(){
+
+};
+
+Battlefield.prototype.updateBattlefield = function(){
+
+};
+
+Battlefield.prototype.checkVictory = function(){
+
+};
+
+Battlefield.prototype.tick = function(){
+
+}
