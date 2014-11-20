c1efc8718649a6789848bd2916fbdd2cadbeb359
Eric Trexel <etrexel@san.rr.com>
2014-11-19 21:51:36 -0600

    Updated Battlefield class skeleton to reflect game loop.

--- js/Battlefield.js
+++ js/Battlefield.js
@@ -8,38 +8,42 @@ var Battlefield = function(first_faction, second_faction){
 	this.faction2Tanks = [];
 	this.faction1Orders = "";
 	this.faction2Orders = "";
+	this.faction1Reports = "";
+	this.faction2Reports = "";
 };
 
 Battlefield.prototype.generateBattlefield = function(){
 
 };
 
-Battlefield.prototype.setupTeam = function(in1, st1){
+Battlefield.prototype.setupTeam = function(faction, json){
 
 };
 
-Battlefield.prototype.setupTeam = function(in1){
+Battlefield.prototype.tankReport = function(faction){
 
 };
 
-Battlefield.prototype.tankReport = function(in1){
+Battlefield.prototype.objectReport = function(faction){
 
 };
 
-Battlefield.prototype.objectReport = function(in1, st1){
-
+Battlefield.prototype.generateReports = function(){
+	var tankReport = this.tankReport(this.faction1);
+	var objectReport = this.objectReport(this.faction1);
+	this.faction1Reports = tankReport + objectReport;
+	tankReport = this.tankReport(this.faction2);
+	objectReport = this.objectReport(this.faction2);
+	this.faction2Reports = tankReport + objectReport;
 };
 
-Battlefield.prototype.loadOrders = function(in1, st1){
-
-};
-
-Battlefield.prototype.executeorders = function(){
+Battlefield.prototype.executeOrders = function(){
 
 };
 
 Battlefield.prototype.clearOrders = function(){
-
+	this.faction1Orders = "";
+	this.faction2Orders = "";
 };
 
 Battlefield.prototype.updateBattlefield = function(){
@@ -47,9 +51,21 @@ Battlefield.prototype.updateBattlefield = function(){
 };
 
 Battlefield.prototype.checkVictory = function(){
+	return null;
+};
+
+Battlefield.prototype.sendReports = function(){
 
 };
 
 Battlefield.prototype.tick = function(){
-
-}
+	this.executeOrders();
+	this.clearOrders();
+	this.updateBattlefield();
+	var victory = this.checkVictory();
+	if(victory != null)
+		return victory;
+	this.generateReports();
+	this.sendReports();
+	return null;
+};
