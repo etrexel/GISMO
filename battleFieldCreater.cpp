9733e648b805f5c71a2478a89d2dc2df7e6a969b
michaelfschem@gmail.com <michaelfschem@gmail.com>
2014-11-27 12:07:17 -0600

    board gen complete

--- battleFieldCreater.cpp
+++ battleFieldCreater.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
+#include "DeckOfCards.h"
+#include "Hand.h"
+#include "Card.h"
+
 using namespace std;
 
 int main () {
--- js/Battlefield.js
+++ js/Battlefield.js
@@ -1,31 +1,55 @@
 var Battlefield = function (first_faction, second_faction) {
-    this.battlefield = [];
-    this.faction1 = first_faction;
-    this.faction2 = second_faction;
-    this.faction1Base = "";
-    this.faction2Base = "";
-    this.faction1Tanks = [];
-    this.faction2Tanks = [];
-    this.faction1Orders = "";
-    this.faction2Orders = "";
-    this.faction1Reports = "";
-    this.faction2Reports = "";
+	this.battlefield = [];
+	this.faction1 = first_faction;
+	this.faction2 = second_faction;
+	this.faction1Base = "";
+	this.faction2Base = "";
+	this.faction1Tanks = [];
+	this.faction2Tanks = [];
+	this.faction1Orders = "";
+	this.faction2Orders = "";
+	this.faction1Reports = "";
+	this.faction2Reports = "";
 
 };
 
 Battlefield.prototype.generateBattlefield = function () {
-    var boardSize = 250;
+	var boardSize = 250;
 
-    this.battlefield = Array(boardSize);
-    for(y = 0 ; y < boardSize ; y++ ) {
-        this.battlefield[y] = Array(boardSize);
-        for(x = 0 ; x < boardSize ; x++ ) {
-            var rand = Math.floor(Math.random() * 1000);
-            this.battlefield[y][x] = 0;
-        }
-    }
+	this.battlefield = Array(boardSize);
+	for(y = 0 ; y < boardSize ; y++ ) {
+		this.battlefield[y] = Array(boardSize);
+		for(x = 0 ; x < boardSize ; x++ ) {
+			if(x > 2 && y > 2){
+				var rand = Math.floor(Math.random() * 1000);
 
-    console.log(this.battlefield);
+				if(rand === 0 || rand === 1){
+					this.battlefield[y][x] = 1;
+					this.battlefield[y - Math.floor(Math.random() * 10) % 2 ][x - Math.floor(Math.random() * 10) % 2 ] = 1;
+				} else if(rand === 2) {
+					this.battlefield[y][x] = 2;
+					this.battlefield[y - Math.floor(Math.random() * 10) % 2 ][x - Math.floor(Math.random() * 10) % 2 ] = 2;
+				} else if(rand === 3) {
+					this.battlefield[y][x] = 3;
+					this.battlefield[y - Math.floor(Math.random() * 10) % 2 ][x - Math.floor(Math.random() * 10) % 2 ] = 3;
+				} else {
+					this.battlefield[y][x] = 0;
+					this.battlefield[y - Math.floor(Math.random() * 10) % 2 ][x - Math.floor(Math.random() * 10) % 2 ] = 0;
+				}
+			} else {
+				this.battlefield[y][x] = 0;
+			}
+		}
+	}
+	for(i = 0; i < 10; i++){
+		for(y = 2; y < boardSize; y ++){
+			for(x = 2; x < boardSize; x++){
+				if(this.battlefield[x][y] != 0){
+					this.battlefield[x - Math.floor(Math.random() * 10) % 2 ][y - Math.floor(Math.random() * 10) % 2 ] = this.battlefield[x][y];
+				}
+			}
+		}
+	}
 };
 
 Battlefield.prototype.setupTeam = function (faction, json) {
@@ -40,12 +64,12 @@ Battlefield.prototype.objectReport = function (faction) {
 };
 
 Battlefield.prototype.generateReports = function () {
-    var tankReport = this.tankReport(this.faction1);
-    var objectReport = this.objectReport(this.faction1);
-    this.faction1Reports = tankReport + objectReport;
-    tankReport = this.tankReport(this.faction2);
-    objectReport = this.objectReport(this.faction2);
-    this.faction2Reports = tankReport + objectReport;
+	var tankReport = this.tankReport(this.faction1);
+	var objectReport = this.objectReport(this.faction1);
+	this.faction1Reports = tankReport + objectReport;
+	tankReport = this.tankReport(this.faction2);
+	objectReport = this.objectReport(this.faction2);
+	this.faction2Reports = tankReport + objectReport;
 };
 
 Battlefield.prototype.executeOrders = function () {
@@ -53,8 +77,8 @@ Battlefield.prototype.executeOrders = function () {
 };
 
 Battlefield.prototype.clearOrders = function () {
-    this.faction1Orders = "";
-    this.faction2Orders = "";
+	this.faction1Orders = "";
+	this.faction2Orders = "";
 };
 
 Battlefield.prototype.updateBattlefield = function () {
@@ -62,7 +86,7 @@ Battlefield.prototype.updateBattlefield = function () {
 };
 
 Battlefield.prototype.checkVictory = function () {
-    return null;
+	return null;
 };
 
 Battlefield.prototype.sendReports = function () {
@@ -70,14 +94,14 @@ Battlefield.prototype.sendReports = function () {
 };
 
 Battlefield.prototype.tick = function () {
-    this.executeOrders();
-    this.clearOrders();
-    this.updateBattlefield();
-    var victory = this.checkVictory();
-    if (victory !== null) {
-        return victory;
-    }
-    this.generateReports();
-    this.sendReports();
-    return null;
+	this.executeOrders();
+	this.clearOrders();
+	this.updateBattlefield();
+	var victory = this.checkVictory();
+	if (victory !== null) {
+		return victory;
+	}
+	this.generateReports();
+	this.sendReports();
+	return null;
 };
